7a422c8e44204c9e5040497fb4018f8b59d73a17
Maia Teegarden <dev@padmaia.rocks>
2021-08-06 18:51:08 -0700

    Add publish workflow

---
+++ scripts/prepublish-native.sh
@@ -0,0 +1,4 @@
+cp ./packages/next/native/next-swc.darwin-arm64.node ./packages/next-swc-darwin-arm64/next-swc.darwin-arm64.node
+cp ./packages/next/native/next-swc.darwin-x64.node ./packages/next-swc-darwin-x64/next-swc.darwin-x64.node
+cp ./packages/next/native/next-swc.linux-x64-gnu.node ./packages/next-swc-linux-x64-gnu/next-swc.linux-x64-gnu.node
+cp ./packages/next/native/next-swc.win32-x64-msvc.node ./packages/next-swc-win32-x64-msvc/next-swc.win32-x64-msvc.node
